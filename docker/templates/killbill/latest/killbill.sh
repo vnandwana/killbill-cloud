f13b842d6ce28f57ac929b1516ff020476163cf4
Vijay N <vijaynandwana7@gmail.com>
2026-05-08 16:16:18 +0530

    Updated Docker image build command to not use cache

--- docker/templates/killbill/latest/killbill.sh
+++ docker/templates/killbill/latest/killbill.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -ex
+
+echo "STARTING INSTALL_PLUGINS_CMD"
+
+
 # Run both the main playbook and the one enabling structured logging
 $KPM_INSTALL_CMD $KILLBILL_CLOUD_ANSIBLE_ROLES/killbill_json_logging.yml
+
+echo "STARTING INSTALL_PLUGINS_CMD..."
+echo "$INSTALL_PLUGINS_CMD"
 
 $INSTALL_PLUGINS_CMD
 
