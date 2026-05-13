b6cfc396db18eb016aa7e7f1f9eb7715805b5311
Vijay N <vijaynandwana7@gmail.com>
2026-05-13 15:37:14 +0530

    Updated killbill.sh script

--- docker/templates/killbill/latest/killbill.sh
+++ docker/templates/killbill/latest/killbill.sh
@@ -2,24 +2,33 @@
 
 set -ex
 
-echo "STARTING INSTALL_PLUGINS_CMD"
+echo "STARTING KPM INSTALL"
 
-
-# Run both the main playbook and the one enabling structured logging
 $KPM_INSTALL_CMD $KILLBILL_CLOUD_ANSIBLE_ROLES/killbill_json_logging.yml
-
-echo "STARTING INSTALL_PLUGINS_CMD..."
-
-until mysql --protocol=TCP -h127.0.0.1 -P3306 -uroot -pkillbill -e "SELECT 1"; do
-  echo "Waiting for MariaDB..."
-  sleep 5
-done
-
-echo "$INSTALL_PLUGINS_CMD"
-
-$INSTALL_PLUGINS_CMD
 
 originalfile=$KILLBILL_INSTALL_DIR/config/shiro.ini.template
 cat $originalfile | envsubst '${KB_ADMIN_PASSWORD}' > $KILLBILL_INSTALL_DIR/config/shiro.ini
 
-exec /usr/share/tomcat/bin/catalina.sh run
+echo "Starting Tomcat..."
+
+/usr/share/tomcat/bin/catalina.sh run &
+
+TOMCAT_PID=$!
+
+echo "Waiting for MariaDB..."
+
+until mysql --protocol=TCP -h127.0.0.1 -P3306 -uroot -pkillbill -e "SELECT 1"; do
+  sleep 5
+done
+
+echo "Waiting for Kill Bill readiness..."
+
+until curl -f http://127.0.0.1:8080/1.0/healthcheck; do
+  sleep 10
+done
+
+echo "STARTING INSTALL_PLUGINS_CMD"
+
+$INSTALL_PLUGINS_CMD
+
+wait $TOMCAT_PID
