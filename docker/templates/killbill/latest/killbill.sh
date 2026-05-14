b271ecab7622e5ae81fb0e73df5f770d19acf05b
Vijay N <vijaynandwana7@gmail.com>
2026-05-14 11:25:26 +0530

    Moved DB variables declaration from INSTALL_PLUGINS_CMD to killbill.sh.

--- docker/templates/killbill/latest/killbill.sh
+++ docker/templates/killbill/latest/killbill.sh
@@ -2,33 +2,32 @@
 
 set -ex
 
-echo "STARTING KPM INSTALL"
+echo "STARTING INSTALL_PLUGINS_CMD"
 
+
+# Run both the main playbook and the one enabling structured logging
 $KPM_INSTALL_CMD $KILLBILL_CLOUD_ANSIBLE_ROLES/killbill_json_logging.yml
+
+echo "STARTING INSTALL_PLUGINS_CMD..."
+
+until mysql --protocol=TCP -h127.0.0.1 -P3306 -uroot -pkillbill -e "SELECT 1"; do
+  echo "Waiting for MariaDB..."
+  sleep 5
+done
+
+
+INSTALL_PLUGINS_CMD="$INSTALL_PLUGINS_CMD \
+  -e db_config_address=127.0.0.1 \
+  -e db_config_port=3306 \
+  -e db_config_username=$KILLBILL_DAO_USER \
+  -e db_config_password=$KILLBILL_DAO_PASSWORD \
+  -e db_config_killbill_db_name=killbill"
+
+echo "$INSTALL_PLUGINS_CMD"
+
+eval "$INSTALL_PLUGINS_CMD"
 
 originalfile=$KILLBILL_INSTALL_DIR/config/shiro.ini.template
 cat $originalfile | envsubst '${KB_ADMIN_PASSWORD}' > $KILLBILL_INSTALL_DIR/config/shiro.ini
 
-echo "Starting Tomcat..."
-
-/usr/share/tomcat/bin/catalina.sh run &
-
-TOMCAT_PID=$!
-
-echo "Waiting for MariaDB..."
-
-until mysql --protocol=TCP -h127.0.0.1 -P3306 -uroot -pkillbill -e "SELECT 1"; do
-  sleep 5
-done
-
-echo "Waiting for Kill Bill readiness..."
-
-until curl -f http://127.0.0.1:8080/1.0/healthcheck; do
-  sleep 10
-done
-
-echo "STARTING INSTALL_PLUGINS_CMD"
-
-$INSTALL_PLUGINS_CMD
-
-wait $TOMCAT_PID
+exec /usr/share/tomcat/bin/catalina.sh run
